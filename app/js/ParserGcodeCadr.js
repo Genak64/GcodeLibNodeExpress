52a34ae95e08d9749b3fd9b1c2309cbc32a09942
Genak64 <genak64@ukr.net>
2022-06-23 20:12:56 +0300

    upd

---
+++ app/js/ParserGcodeCadr.js
@@ -0,0 +1,32 @@
+
+
+	
+	function parseXY (item){
+	var point={};
+	item=item.toUpperCase();
+	point.x=parseFloat(parseX(item));
+	point.y=parseFloat(parseY(item));
+	return point;
+	}
+
+	function parseS (item){
+	item=item.toUpperCase();
+	return parseFloat(item.slice(item.indexOf("S")+1).replace(/[,]+/g, '.')).toFixed(2);
+	}
+
+	function parseF (item){
+	item=item.toUpperCase();
+	return parseFloat(item.slice(item.indexOf("F")+1).replace(/[,]+/g, '.')).toFixed(2);
+	}
+
+	function parseX (item){
+	item=item.toUpperCase();
+	return parseFloat(item.slice(item.indexOf("X")+1).replace(/[,]+/g, '.')).toFixed(2);
+	}
+
+	function parseY (item){
+	item=item.toUpperCase();
+	return parseFloat(item.slice(item.indexOf("Y")+1).replace(/[,]+/g, '.')).toFixed(2);
+	}
+
+module.exports={parseXY,parseS,parseF,parseX,parseY};
---
+++ app/test/gcodeToShapeModule.test.js
@@ -0,0 +1,6 @@
+const assert = require('assert');
+const gcodeToShape = require('../js/gcodeToShapeModule.js');
+
+it('gcodeToShapeModule должно возвращать true', () => {
+  assert.equal(true, true);
+});
