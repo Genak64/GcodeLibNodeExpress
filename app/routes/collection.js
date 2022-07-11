1e6ab2a0510c087df7fee9c5d39c8b79b8f4a8c7
Genak64 <genak64@ukr.net>
2022-07-11 23:31:27 +0300

    upd

--- app/routes/collection.js
+++ app/routes/collection.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+const library = require('js-svg-path');
+
+var sp = require('../js/svgToShape.js');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	
+	sp.svgpr();
+	
+	
   res.render('collection', { title: 'G-code viewer' });
 });
 
